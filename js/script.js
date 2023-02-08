12717e193346c2c42671692fea6c75260b5cca2b
godinetcKirill <117347199+godinetcKirill@users.noreply.github.com>
2023-02-08 21:46:47 +0300

    remove css

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const swiper = new Swiper('.slider-main-block', {
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-main-block__arrow.swiper-button-next',
-    prevEl: '.slider-main-block__arrow.swiper-button-prev',
+    nextEl: '.body-main-block__arrow.swiper-button-next',
+    prevEl: '.body-main-block__arrow.swiper-button-prev',
   },
 });
